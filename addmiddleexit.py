9b73a7aa4a63ad69ae994428590061541c6e418d
cyberdan34 <lipe98t@gmail.com>
2020-12-10 00:16:21 +0000

    settings

--- addmiddleexit.py
+++ addmiddleexit.py
@@ -6,10 +6,10 @@ Created on Mon Dec  9 13:01:52 2019
 @author: filipeneto
 """
 
-#Allow the user enter a sequence non profit 
-# numbers . the user ends the list with a negative 
-# number at the end the sum of the non negative 
-# nmvere entered os displayed the program prints 
+#Allow the user enter a sequence non profit
+# numbers . the user ends the list with a negative
+# number at the end the sum of the non negative
+# nmvere entered os displayed the program prints
 # zero if the user provides no negative numbers
 
 entry = 0
@@ -23,5 +23,5 @@ while True:                    #Loop forever
     if entry < 0:              #Is number negative number?
         break                  #If so, exit the loop
     sum += entry               #Add entry ti running sum
-    
+
 print("Sum =", sum)
